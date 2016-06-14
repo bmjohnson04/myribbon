6684b8bef0bbebe29de0a8109dbf625a49025eb9
Brandon <bmjohnson04@gmail.com>
2016-06-13 22:44:31 -0400

    added route to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/front'
+  root to: 'home#front'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
