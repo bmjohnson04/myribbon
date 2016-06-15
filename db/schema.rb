a71093fe229e049f769758ff17cbdaa7d9ed6d8a
Brandon <bmjohnson04@gmail.com>
2016-06-15 19:00:39 -0400

    successfully migrated name column to after rake db:drop db:create db:migrate also and adminrb email too devise.rb config initializer file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615212024) do
+ActiveRecord::Schema.define(version: 20160615200245) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "attachment"
@@ -24,8 +24,12 @@ ActiveRecord::Schema.define(version: 20160615212024) do
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
+    t.string   "name",                   default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "about"
+    t.string   "avatar"
+    t.string   "cover"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -34,11 +38,14 @@ ActiveRecord::Schema.define(version: 20160615212024) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
