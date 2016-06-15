e68b4faa608049b19c3115f515ef1e1941b38d48
Brandon <bmjohnson04@gmail.com>
2016-06-15 17:50:06 -0400

    added the name column block argument to change method to migration

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160615212024) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
