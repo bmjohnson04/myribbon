b0866b6013e09466c65201eade1e901d94f5a22b
Brandon <bmjohnson04@gmail.com>
2016-06-15 17:24:12 -0400

    add name to user table becaus e it was not added at initial creation and also resolved undefined method name error.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615200245) do
+ActiveRecord::Schema.define(version: 20160615212024) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "attachment"
