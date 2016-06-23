1a16e4634c92e799dfbb10b1903ee571a2f0f087
Brandon <bmjohnson04@gmail.com>
2016-06-22 23:06:57 -0400

    added column comment_html to comment table to fix abililty to leave comment on followable user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621225229) do
+ActiveRecord::Schema.define(version: 20160623030321) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20160621225229) do
     t.string   "role",                        default: "comments"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "comment_html"
   end
 
   add_index "comments", ["commentable_id"], name: "index_comments_on_commentable_id"
